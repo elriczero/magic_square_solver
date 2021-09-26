6694b98a1d6116570882e626cd0b2503c3398026
elriczero <dark_army96@hotmail.com>
2021-09-26 18:50:58 -0500

    Add Initial Commit to resolve the problem

---
+++ main.py
@@ -0,0 +1,17 @@
+#initialise empty 3 by 3 grid
+grid = []
+grid.append([8,1,0])
+grid.append([0,5,7])
+grid.append([4,9,0])
+
+
+
+def display_grid(grid):
+    for i in range(3):
+        print(grid[i])
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
