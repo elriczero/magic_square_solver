aec2927bc7e72c994932fe9167ee1d11b4343728
elriczero <dark_army96@hotmail.com>
2021-09-26 19:16:01 -0500

    Added sum of Rows Display element

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-#initialise empty 3 by 3 grid
+# initialise empty 3 by 3 grid
 grid = []
-grid.append([8,1,0])
-grid.append([0,5,7])
-grid.append([4,9,0])
-
+grid.append([8, 1, 0])
+grid.append([0, 5, 7])
+grid.append([4, 9, 0])
 
 
 def display_grid(grid):
@@ -11,7 +10,27 @@ def display_grid(grid):
         print(grid[i])
 
 
+def sum_rows(grid):
+    f_row = grid[0]
+    s_row = grid[1]
+    t_row = grid[2]
+    sum_f_row = 0
+    sum_s_row = 0
+    sum_t_row = 0
+    for i in f_row:
+        sum_f_row += i
+    for i in s_row:
+        sum_s_row += i
+    for i in t_row:
+        sum_t_row += i
+    sum_rows_list = [sum_f_row, sum_s_row, sum_t_row]
+    return sum_rows_list
+
+def sum_col(grid):
+    for i in range(3):
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    display_grid(grid)
+    print(sum_rows(grid))
